3b06163aa0801a4a2c6489e637bae4e0d9eb5a57
JanooGwan <janoogwan39@gmail.com>
2025-11-08 22:51:45 +0900

    feat: ArticeRequestDto, ArticeResponseDto 작성

--- src/main/java/com/example/backend_2025_2/domain/dto/ArticleRequestDto.java
+++ src/main/java/com/example/backend_2025_2/domain/dto/ArticleRequestDto.java
@@ -2,11 +2,12 @@ package com.example.backend_2025_2.domain.dto;
 
 import com.example.backend_2025_2.domain.Article;
 
+
 public record ArticleRequestDto (
         String title,
         String content
 ) {
-    public static Article toEntity() {
-        return new Article
+    public Article toEntity() {
+        return new Article(title, content);
     }
 }
