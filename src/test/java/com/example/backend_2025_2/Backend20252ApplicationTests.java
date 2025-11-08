f5ba3e9c905705e7243c67e8fab2b2d1ba576a68
JanooGwan <janoogwan39@gmail.com>
2025-11-08 14:07:19 +0900

    feat: HelloController 작성

---
+++ src/test/java/com/example/backend_2025_2/Backend20252ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_2025_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Backend20252ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
