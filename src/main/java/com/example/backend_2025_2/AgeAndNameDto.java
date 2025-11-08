86069a2456945192f6989c68d1828852b48017ab
JanooGwan <janoogwan39@gmail.com>
2025-11-08 14:35:45 +0900

    feat: AgeAndNameDto 작성

---
+++ src/main/java/com/example/backend_2025_2/AgeAndNameDto.java
@@ -0,0 +1,6 @@
+package com.example.backend_2025_2;
+
+public record AgeAndNameDto (
+        int age,
+        String name
+) {}
---
+++ src/main/java/com/example/backend_2025_2/JsonController.java
@@ -0,0 +1,12 @@
+package com.example.backend_2025_2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JsonController {
+
+    @GetMapping("/json") {
+
+    }
+}
