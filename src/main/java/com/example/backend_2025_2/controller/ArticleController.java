0627c1d1cedc538e8b59bd83e531a97553d13a97
JanooGwan <janoogwan39@gmail.com>
2025-11-09 00:28:30 +0900

    feat: ArticleController 작성

--- src/main/java/com/example/backend_2025_2/controller/ArticleController.java
+++ src/main/java/com/example/backend_2025_2/controller/ArticleController.java
@@ -1,19 +1,42 @@
 package com.example.backend_2025_2.controller;
 
-import com.example.backend_2025_2.domain.Article;
+import com.example.backend_2025_2.domain.dto.ArticleCreateRequestDto;
 import com.example.backend_2025_2.domain.dto.ArticleResponseDto;
+import com.example.backend_2025_2.domain.dto.ArticleUpdateRequestDto;
+import com.example.backend_2025_2.service.ArticleService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/articles")
 public class ArticleController {
 
-    @GetMapping("/{id}")
-    public ResponseEntity<ArticleResponseDto> getArticle(@PathVariable("id") Long id) {
+    private final ArticleService articleService = new ArticleService();
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<ArticleResponseDto>> getArticle(@PathVariable("id") Long id) {
+        Optional<ArticleResponseDto> response = articleService.getArticleById(id);
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping
+    public ResponseEntity<ArticleResponseDto> createArticle(@RequestBody ArticleCreateRequestDto articleCreateRequestDto) {
+        ArticleResponseDto response = articleService.createArticle(articleCreateRequestDto);
+        return ResponseEntity.ok(response);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Optional<ArticleResponseDto>> updateArticle(@PathVariable Long id, @RequestBody ArticleUpdateRequestDto articleUpdateRequestDto) {
+        Optional<ArticleResponseDto> response = articleService.updateArticle(id, articleUpdateRequestDto);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteArticleById(@PathVariable Long id) {
+        articleService.deleteArticle(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
