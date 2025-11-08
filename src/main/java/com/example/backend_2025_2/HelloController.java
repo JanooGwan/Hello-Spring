531b0cc89d44273c71672ea25eed2e187fb4564e
JanooGwan <janoogwan39@gmail.com>
2025-11-08 14:32:25 +0900

    refactor: introduce() - @RequestParam 활용한 형태로 수정

--- src/main/java/com/example/backend_2025_2/HelloController.java
+++ src/main/java/com/example/backend_2025_2/HelloController.java
@@ -1,14 +1,15 @@
 package com.example.backend_2025_2;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
-
     @GetMapping("/introduce")
-    public String introduce() {
+    public String introduce(@RequestParam(name = "name", defaultValue = "이관우") String name, Model model) {
+        model.addAttribute("name", name);
         return "introduction";
     }
 }
