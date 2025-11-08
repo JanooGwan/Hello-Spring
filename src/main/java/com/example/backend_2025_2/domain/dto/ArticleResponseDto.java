f9b66039e8e81e1b51dc84db704035f921a44e4b
JanooGwan <janoogwan39@gmail.com>
2025-11-09 00:10:43 +0900

    refactor: ArticleResponseDto - 정적 팩토리 메소드(from) 추가

--- src/main/java/com/example/backend_2025_2/domain/dto/ArticleResponseDto.java
+++ src/main/java/com/example/backend_2025_2/domain/dto/ArticleResponseDto.java
@@ -1,5 +1,7 @@
 package com.example.backend_2025_2.domain.dto;
 
+import com.example.backend_2025_2.domain.Article;
+
 import java.time.LocalDateTime;
 
 public record ArticleResponseDto (
@@ -8,4 +10,14 @@ public record ArticleResponseDto (
         String content,
         LocalDateTime createdDate,
         LocalDateTime modifiedDate
-) {}
+) {
+    public static ArticleResponseDto from(Article article) {
+        return new ArticleResponseDto(
+                article.getId(),
+                article.getTitle(),
+                article.getContent(),
+                article.getCreatedDate(),
+                article.getModifiedDate()
+        );
+    }
+}
---
+++ src/main/java/com/example/backend_2025_2/service/ArticleService.java
@@ -0,0 +1,15 @@
+package com.example.backend_2025_2.service;
+
+import com.example.backend_2025_2.domain.Article;
+import com.example.backend_2025_2.domain.dto.ArticleCreateRequestDto;
+import com.example.backend_2025_2.domain.dto.ArticleResponseDto;
+import com.example.backend_2025_2.domain.dto.ArticleUpdateRequestDto;
+import com.example.backend_2025_2.repository.ArticleRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class ArticleService {
+
+}
